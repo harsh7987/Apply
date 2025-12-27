10f2b7e7aa719d78452bb3639ebad3a446507fcb
Harsh <harshvermacoc123@gmail.com>
2025-12-27 16:09:59 +0530

    Initial Commit

---
+++ Apply/ApplyApp.swift
@@ -0,0 +1,17 @@
+//
+//  ApplyApp.swift
+//  Apply
+//
+//  Created by Pranjal Verma on 27/12/25.
+//
+
+import SwiftUI
+
+@main
+struct ApplyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Apply/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Apply
+//
+//  Created by Pranjal Verma on 27/12/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
